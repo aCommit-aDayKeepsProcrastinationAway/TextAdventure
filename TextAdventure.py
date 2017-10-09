106c7b95fc10101b47f7d90a6652dbd9ba813cd3
aCommit-aDayKeepsProcrastinationAway <baltalukas1@yahoo.com>
2017-10-09 12:46:12 +0000

    Added a function for easier dialog and expanded the story a bit

--- TextAdventure.py
+++ TextAdventure.py
@@ -1,3 +1,13 @@
+def DialogOptions(Options):
+  count = 96
+  print("--------------------------------------------------------------")
+  print("Do you: ")
+  for Option in Options:
+    count += 1
+    character = chr(count)
+    print("%s) %s"%(character, Option))
+  print("--------------------------------------------------------------")
+  
 print("----------121COM Week 2 Lab Exercise 7----------")
 print("----------------<InsertTitleHere>---------------")
 print("---------------<Lukas>--------------")
@@ -9,11 +19,12 @@ print("b) Press the light switch")
 x = input("Enter a or b: ")
 if x == "a":
     print("Someone hears your screams...")
-    # Contine adventure Here
+    # Another branch
 elif x == "b":
     print("The light comes on.")
     print("You do not recognise the room but you have a really bad feeling...")
-    # Contine adventure Here
+    DialogOptions(["Look around", "Inspect the bed"])
+    if x == 
 else:
     print("That was not an option.  Game Over")
 
